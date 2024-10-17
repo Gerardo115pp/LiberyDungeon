a483df766b984091a2735d7c58c904d966c4b315
Gerardo <theronin115@gmail.com>
2024-10-16 19:44:32 -0600

    Significantly reduced the time the hotkeys binder delay key repetition as the features this did improved, can be also improved by single typing. but there was no way around it for the features it did affected

--- apps/libery_dungeon/src/libs/LiberyHotkeys/hotkeys_consts.js
+++ apps/libery_dungeon/src/libs/LiberyHotkeys/hotkeys_consts.js
@@ -18,7 +18,7 @@
     export const MAX_PAST_EVENTS = 100;
     export const MAX_PAST_HOTKEYS_TRIGGERED = 10;
     export const MAX_TIME_BETWEEN_SEQUENCE_KEYSTROKES = 1200; // Milliseconds
-    export const MIN_TIME_BETWEEN_HOTKEY_REPEATS = 130; // Milliseconds
+    export const MIN_TIME_BETWEEN_HOTKEY_REPEATS = 20; // Milliseconds
     // Whether to check all hotkeys with the same trigger and pick the longest one(true) or just the first one that matches(false)
     export const HOTKEY_SPECIFICITY_PRECEDENCE = true; 
     export const DISABLE_KEYPRESS_EVENTS = true;
