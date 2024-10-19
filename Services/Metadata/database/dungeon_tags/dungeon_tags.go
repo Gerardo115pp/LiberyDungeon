fb2d25059bd2dac29980fdf1e60880bd6ad59f56
Gerardo <theronin115@gmail.com>
2024-10-19 02:05:43 -0600

    Added methods to create all different dungeon_tag models

--- Services/Metadata/database/dungeon_tags/dungeon_tags.go
+++ Services/Metadata/database/dungeon_tags/dungeon_tags.go
@@ -1,6 +1,10 @@
 package dungeon_tags
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+	service_models "libery-metadata-service/models"
+)
 
 type DungeonTagsDB struct {
 	db_conn *sql.DB
@@ -19,4 +23,50 @@ func NewDungeonTagsDB() (*DungeonTagsDB, error) {
 	db.Exec("PRAGMA foreign_keys = ON")
 
 	return users_db, nil
+}
+
+func (dt_db *DungeonTagsDB) CreateTaxonomyCTX(ctx context.Context, taxonomy *service_models.TagTaxonomy) error {
+	stmt, err := dt_db.db_conn.PrepareContext(ctx, "INSERT INTO `tag_taxonomies`(`uuid`, `name`, `cluster_domain`) VALUES (?, ?, ?)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.ExecContext(ctx, taxonomy.UUID, taxonomy.Name, taxonomy.ClusterDomain)
+
+	return err
+}
+
+func (dt_db *DungeonTagsDB) CreateTaxonomy(taxonomy *service_models.TagTaxonomy) error {
+	return dt_db.CreateTaxonomyCTX(context.Background(), taxonomy)
+}
+
+func (dt_db *DungeonTagsDB) CreateTagCTX(ctx context.Context, tag *service_models.DungeonTag) error {
+	stmt, err := dt_db.db_conn.PrepareContext(ctx, "INSERT INTO `dungeon_tags`(`name`, `taxonomy`, `name_taxonomy`) VALUES (?, ?, ?)")
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.ExecContext(ctx, tag.Name, tag.Taxonomy, tag.NameTaxonomy)
+
+	return err
+}
+
+func (dt_db *DungeonTagsDB) CreateTag(tag *service_models.DungeonTag) error {
+	return dt_db.CreateTagCTX(context.Background(), tag)
+}
+
+func (dt_db *DungeonTagsDB) TagEntityCTX(ctx context.Context, tag_id int, entity_uuid string) error {
+	stmt, err := dt_db.db_conn.PrepareContext(ctx, "INSERT INTO `taggings`(`tag_id`, `tagged_entity_uuid`) VALUES (?, ?)")
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.ExecContext(ctx, tag_id, entity_uuid)
+
+	return err
+}
+
+func (dt_db *DungeonTagsDB) TagEntity(tag_id int, entity_uuid string) error {
+	return dt_db.TagEntityCTX(context.Background(), tag_id, entity_uuid)
 }
