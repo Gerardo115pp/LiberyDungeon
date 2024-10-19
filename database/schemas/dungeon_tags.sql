c897daa15bd62b834c29c4fb168e4bc69c267496
Gerardo <theronin115@gmail.com>
2024-10-19 01:23:45 -0600

    Created a sqlite database schema for The Tag system

---
+++ database/schemas/dungeon_tags.sql
@@ -0,0 +1,25 @@
+PRAGMA foreign_keys=ON;
+
+DROP TABLE IF EXISTS `tag_taxonomies`;
+CREATE TABLE IF NOT EXISTS `tag_taxonomies` (
+    `uuid` TEXT PRIMARY KEY,
+    `name` TEXT NOT NULL,
+    `cluster_domain` TEXT NOT NULL
+);
+
+DROP TABLE IF EXISTS `dungeon_tags`;
+CREATE TABLE IF NOT EXISTS `dungeon_tags` (
+    `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `name` TEXT NOT NULL,
+    `taxonomy` TEXT NOT NULL,
+    `name-taxonomy` TEXT NOT NULL UNIQUE,
+    FOREGIN KEY(`taxonomy`) REFERENCES `tag_taxonomies`(`uuid`) ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS `taggings`;
+CREATE TABLE IF NOT EXISTS `taggings` (
+    `tagging_id` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `tag` INTEGER NOT NULL,
+    `taggable_id` TEXT NOT NULL,
+    FOREIGN KEY(`tag`) REFERENCES `dungeon_tags`(`id`) ON DELETE CASCADE
+);
