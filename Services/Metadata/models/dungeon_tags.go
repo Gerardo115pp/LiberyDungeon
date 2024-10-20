a02703697f6d80c5ed5190d221f4544341086afe
Gerardo <theronin115@gmail.com>
2024-10-20 00:13:55 -0600

    Add IsInternal field to TagTaxonomy model for internal usage flag

--- Services/Metadata/models/dungeon_tags.go
+++ Services/Metadata/models/dungeon_tags.go
@@ -4,6 +4,7 @@ type TagTaxonomy struct {
 	UUID          string `json:"uuid"`
 	Name          string `json:"name"`
 	ClusterDomain string `json:"cluster_domain"` // If not an empty string, defines the cluster where the tags on this taxonomy are applied
+	IsInternal    bool   `json:"is_internal"`    // If true, this taxonomy is only used internally by the system and not exposed to the user
 }
 
 type TaxonomyTags struct {
