2f553639d15737e19cd22b7f2c40ad2bfbd761a4
Gerardo <theronin115@gmail.com>
2024-10-21 00:22:26 -0600

    Splited the services url declarations into their own file

---
+++ apps/libery_dungeon/src/libs/DungeonRequests/services.js
@@ -0,0 +1,9 @@
+export const pw_server = PW_SERVER;
+export const base_domain = BASE_DOMAIN;
+export const categories_server = CATEGORIES_SERVER;
+export const medias_server = MEDIAS_SERVER;
+export const collect_server = COLLECT_SERVER;
+export const downloads_server = DOWNLOADS_SERVER;
+export const metadata_server = METADATA_SERVER;
+export const jd_server = JD_SERVER; 
+export const users_server = USERS_SERVER;
