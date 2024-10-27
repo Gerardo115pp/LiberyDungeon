c239f19a950e5c38bd012e309ed97b5ade4a8854
Gerardo <theronin115@gmail.com>
2024-10-27 15:30:28 -0600

    feat: add entity types to dungeon_tags_config

---
+++ apps/libery_dungeon/src/config/dungeon_tags_config.js
@@ -0,0 +1,12 @@
+
+/*=============================================
+=            Entity types            =
+=============================================*/
+
+    export const CATEGORY_ENTITY_TYPE = "category";
+    export const DUNGEON_ENTITY_TYPE = "dungeon";
+    export const MEDIA_ENTITY_TYPE = "media";
+    export const TRACK_ENTITY_TYPE = "track"; // spoiler of what's coming next...
+
+/*=====  End of Entity types  ======*/
+
