56fb7df5d2fa12dd2fc853c9ce295e0086ecdeee
Gerardo <theronin115@gmail.com>
2024-10-27 14:27:46 -0600

    feat: add entity_type column to taggings table;

--- database/schemas/dungeon_tags.sql
+++ database/schemas/dungeon_tags.sql
@@ -1,5 +1,3 @@
-PRAGMA foreign_keys=ON;
-
 DROP TABLE IF EXISTS `tag_taxonomies`;
 CREATE TABLE IF NOT EXISTS `tag_taxonomies` (
     `uuid` TEXT PRIMARY KEY,
@@ -21,7 +19,8 @@ DROP TABLE IF EXISTS `taggings`;
 CREATE TABLE IF NOT EXISTS `taggings` (
     `tagging_id` INTEGER PRIMARY KEY AUTOINCREMENT,
     `tag` INTEGER NOT NULL,
+    `entity_type` TEXT NOT NULL DEFAULT '',
     `taggable_id` TEXT NOT NULL,
     FOREIGN KEY(`tag`) REFERENCES `dungeon_tags`(`id`) ON DELETE CASCADE,
-    UNIQUE(`tag`, `taggable_id`)
+    UNIQUE(`tag`, `taggable_id`) 
 );
