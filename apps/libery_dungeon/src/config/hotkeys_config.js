b0081fb1575822b93169bb871579f3d7936c4602
Gerardo <theronin115@gmail.com>
2024-11-02 21:00:17 -0600

    feat: added a configuration file for hotkeys with global key triggers for common actions

---
+++ apps/libery_dungeon/src/config/hotkeys_config.js
@@ -0,0 +1,20 @@
+/*=============================================
+=            GLOBAL            =
+=============================================*/
+
+    export const global_hotkey_action_triggers = {
+        QUITE_CONTEXT: ["q"],
+        TOGGLE_HOTKEYS_SHEET: ["?"],
+        NAVIGATION_UP: ["w"],
+        NAVIGATION_DOWN: ["s"],
+        NAVIGATION_LEFT: ["a"],
+        NAVIGATION_RIGHT: ["d"],
+        ITEM_SELECTION: ["e"],
+        ITEM_DELETION_NON_IMPERATIVE: ["x"],
+        ITEM_DELETION_IMPERATIVE: ["del"],
+        ITEM_RENAMING: ["c c"],
+        NAVIGATION_ITEM_GOTO_FINALIZER: ["g"],
+    }
+
+/*=====  End of GLOBAL  ======*/
+
