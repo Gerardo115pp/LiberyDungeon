8b6eee5ee91a1157ac2a99ac5b280437314b3d36
Gerardo <theronin115@gmail.com>
2024-11-04 00:25:53 -0600

    feat: added a file for share hotkey action for the taxonomy tags component. now with patterns like this one we can allow other components to know how and what they can overwrite and the component that owns the hotkey context can know what got overwritten and how to handle it. Not a replacement for the common HotkeyContext which is much more straight forward, but this improves and makes somethings possible, to components that have a lot of sub component with their own hotkey context.

---
+++ apps/libery_dungeon/src/components/DungeonTags/TagTaxonomyComponents/TaxonomyTags/taxonomy_tags_hotkeys.js
@@ -0,0 +1,113 @@
+import { ComponentHotkeyContext } from "@libs/LiberyHotkeys/hotkeys_context";
+import { HOTKEY_NULL_DESCRIPTION, HOTKEY_NULLISH_HANDLER } from "@libs/LiberyHotkeys/hotkeys_consts";
+import * as common_hotkey_actions  from "@common/keybinds/CommonActionsName";
+import { global_hotkey_action_triggers, global_hotkey_movement_triggers } from "@config/hotkeys_config";
+
+export const taxonomy_tags_actions = {
+    DROP_HOTKEY_CONTEXT: common_hotkey_actions.DROP_HOTKEY_CONTEXT,
+    WASD_NAVIGATION: common_hotkey_actions.WASD_NAVIGATION,
+    FOCUS_TAG_CREATOR: Symbol("FOCUS_TAG_CREATOR"),
+    RENAME_FOCUSED_TAG: Symbol("RENAME_FOCUSED_TAG"),
+    SELECT_FOCUSED_TAG: Symbol("SELECT_FOCUSED_TAG"),
+    ALT_SELECT_FOCUSED_TAG: Symbol("ALT_SELECT_FOCUSED_TAG"),
+    DELETE_FOCUSED_TAG: Symbol("DELETE_FOCUSED_TAG"),
+    ALT_DELETE_FOCUSED_TAG: Symbol("ALT_DELETE_FOCUSED_TAG"),
+}
+
+const taxonomy_tags_hotkeys = new ComponentHotkeyContext("taxonomy_tags");
+
+taxonomy_tags_hotkeys.registerHotkeyAction(taxonomy_tags_actions.DROP_HOTKEY_CONTEXT, {
+    overwrite_behavior: ComponentHotkeyContext.OVERRIDE_BEHAVIOR_REPLACE,
+    hotkey_register_params: {
+        hotkey_triggers: global_hotkey_action_triggers.QUITE_CONTEXT,
+        callback: HOTKEY_NULLISH_HANDLER,
+        options: {
+            await_execution: false,
+            description: HOTKEY_NULL_DESCRIPTION,
+        },
+    }
+});
+
+taxonomy_tags_hotkeys.registerHotkeyAction(taxonomy_tags_actions.WASD_NAVIGATION, {
+    overwrite_behavior: ComponentHotkeyContext.OVERRIDE_BEHAVIOR_IGNORE,
+    hotkey_register_params: {
+        hotkey_triggers: global_hotkey_movement_triggers,
+        callback: HOTKEY_NULLISH_HANDLER,
+        options: {
+            description: HOTKEY_NULL_DESCRIPTION,
+        },
+    }
+});
+
+taxonomy_tags_hotkeys.registerHotkeyAction(taxonomy_tags_actions.FOCUS_TAG_CREATOR, {
+    overwrite_behavior: ComponentHotkeyContext.OVERRIDE_BEHAVIOR_WRAP,
+    hotkey_register_params: {
+        hotkey_triggers: ["i"],
+        callback: HOTKEY_NULLISH_HANDLER,
+        options: {
+            description: HOTKEY_NULL_DESCRIPTION,
+            await_execution: false,
+            mode: "keyup"
+        },
+    }
+});
+
+taxonomy_tags_hotkeys.registerHotkeyAction(taxonomy_tags_actions.RENAME_FOCUSED_TAG, {
+    overwrite_behavior: ComponentHotkeyContext.OVERRIDE_BEHAVIOR_IGNORE,
+    hotkey_register_params: {
+        hotkey_triggers: global_hotkey_action_triggers.ITEM_RENAMING,
+        callback: HOTKEY_NULLISH_HANDLER,
+        options: {
+            description: HOTKEY_NULL_DESCRIPTION,
+            mode: "keyup"
+        }
+    }
+});
+
+taxonomy_tags_hotkeys.registerHotkeyAction(taxonomy_tags_actions.SELECT_FOCUSED_TAG, {
+    overwrite_behavior: ComponentHotkeyContext.OVERRIDE_BEHAVIOR_IGNORE,
+    hotkey_register_params: {
+        hotkey_triggers: global_hotkey_action_triggers.ITEM_SELECTION,
+        callback: HOTKEY_NULLISH_HANDLER,
+        options: {
+            description: HOTKEY_NULL_DESCRIPTION,
+        }
+    }
+});
+
+taxonomy_tags_hotkeys.registerHotkeyAction(taxonomy_tags_actions.ALT_SELECT_FOCUSED_TAG, {
+    overwrite_behavior: ComponentHotkeyContext.OVERRIDE_BEHAVIOR_REPLACE,
+    hotkey_register_params: {
+        hotkey_triggers: ["shift+e"],
+        callback: HOTKEY_NULLISH_HANDLER,
+        options: {
+            description: HOTKEY_NULL_DESCRIPTION,
+        },
+    }
+});
+
+taxonomy_tags_hotkeys.registerHotkeyAction(taxonomy_tags_actions.DELETE_FOCUSED_TAG, {
+    overwrite_behavior: ComponentHotkeyContext.OVERRIDE_BEHAVIOR_IGNORE,
+    hotkey_register_params: {
+        hotkey_triggers: global_hotkey_action_triggers.ITEM_DELETION_NON_IMPERATIVE,
+        callback: HOTKEY_NULLISH_HANDLER,
+        options: {
+            description: HOTKEY_NULL_DESCRIPTION,
+        },
+    }
+});
+
+taxonomy_tags_hotkeys.registerHotkeyAction(taxonomy_tags_actions.ALT_DELETE_FOCUSED_TAG, {
+    overwrite_behavior: ComponentHotkeyContext.OVERRIDE_BEHAVIOR_REPLACE,
+    hotkey_register_params: {
+        hotkey_triggers: ["shift+x"],
+        callback: HOTKEY_NULLISH_HANDLER,
+        options: {
+            description: HOTKEY_NULL_DESCRIPTION,
+        },
+    }
+});
+
+taxonomy_tags_hotkeys.SetFinal();
+
+export default taxonomy_tags_hotkeys;
