1ddf688b48ce7f12fb392fad61ef0749ca78d017
Gerardo <theronin115@gmail.com>
2024-11-04 00:20:18 -0600

    refactor: rename CommonActions.js to CommonActionsName.js and add SHOW_HOTKEYS_TABLE constant

--- apps/libery_dungeon/src/common/keybinds/CommonActions.js
+++
@@ -1,7 +0,0 @@
-/**
-* Defines common actions that many components have to implement.
- * @module libery_dungeon/src/common/keybinds/CommonActions
-*/
-
-export const DROP_HOTKEY_CONTEXT = Symbol("DROP_HOTKEY_CONTEXT")
-export const WASD_NAVIGATION = Symbol("WASD_NAVIGATION")
---
+++ apps/libery_dungeon/src/common/keybinds/CommonActionsName.js
@@ -0,0 +1,8 @@
+/**
+* Defines common actions that many components have to implement.
+ * @module libery_dungeon/src/common/keybinds/CommonActions
+*/
+
+export const DROP_HOTKEY_CONTEXT = Symbol("DROP_HOTKEY_CONTEXT");
+export const WASD_NAVIGATION = Symbol("WASD_NAVIGATION");
+export const SHOW_HOTKEYS_TABLE = Symbol("SHOW_HOTKEYS_TABLE");
