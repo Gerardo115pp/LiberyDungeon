6459027e760781049b83f98b4404d21086026b2a
Gerardo <theronin115@gmail.com>
2024-11-05 16:37:26 -0600

    refactor: optimize placeholder generation in GetPreparedListPlaceholders function

--- Services/Shared/go_shared/helpers/sql_helpers.go
+++ Services/Shared/go_shared/helpers/sql_helpers.go
@@ -1,11 +1,18 @@
 package helpers
 
+import "strings"
+
 func GetPreparedListPlaceholders(count int) string {
-	var placeholders string = "?"
+	var placeholders string
+
+	placeholders_builder := strings.Builder{}
+	placeholders_builder.WriteString("?")
 
 	for h := 1; h < count; h++ {
-		placeholders += ", ?"
+		placeholders_builder.WriteString(", ?")
 	}
+
+	placeholders = placeholders_builder.String()
 
 	return placeholders
 }
