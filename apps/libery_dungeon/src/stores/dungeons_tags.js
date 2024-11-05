7028b4b74d885db2d8dfd205f0bc14ec8bdab9d0
Gerardo <theronin115@gmail.com>
2024-11-05 14:46:24 -0600

    fix: correct JSDoc type annotation for cluster_tags in dungeons_tags.js

--- apps/libery_dungeon/src/stores/dungeons_tags.js
+++ apps/libery_dungeon/src/stores/dungeons_tags.js
@@ -3,7 +3,7 @@ import { get, writable } from "svelte/store";
 
 /**
 * All the available user-defined tags in the current cluster as list of TaxonomyTags objects.
- * @type {import('svelte/store').Writable<import('@models/DungeonTags').TaxonomyTags[]>>}
+ * @type {import('svelte/store').Writable<import('@models/DungeonTags').TaxonomyTags[]>}
  */
 export const cluster_tags = writable([]);
 
