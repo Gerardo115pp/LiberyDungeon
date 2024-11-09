d3f9a1649d04b5d5ea0e229b892c6b15a428c008
Gerardo <theronin115@gmail.com>
2024-11-09 00:32:28 -0600

    fix: enhance JSDoc annotations and organize properties and methods in MediaGalleryThumbnail component

--- apps/libery_dungeon/src/pages/MediaViewer/sub-components/MediaGallery/MediaGalleryThumbnail.svelte
+++ apps/libery_dungeon/src/pages/MediaViewer/sub-components/MediaGallery/MediaGalleryThumbnail.svelte
@@ -6,49 +6,76 @@
     import { media_change_types } from "@models/WorkManagers";
     import { current_category } from "@stores/categories_tree";
     import { onMount } from "svelte";
+    
+    /*=============================================
+    =            Properties            =
+    =============================================*/
 
+        /**
+         * @type {import('@models/Medias').Media} the media to be displayed
+         */
+        export let media;
 
-    const event_dispatcher = createEventDispatcher();
+        /**
+         * @type {string} the current category path
+         */
+        export let category_path;
 
-    /** @type{import('@models/Medias').Media} the media to be displayed */
-    export let media;
+        /**
+         * @type {boolean} whether the deletion mode is enabled or not 
+         */
+        export let deletion_mode = false;
 
-    /** @type{string} the current category path */
-    export let category_path;
+        /** 
+         * @type {import('@models/WorkManagers').MediaChangeType} the current media change type
+         */
+        let media_current_change = media_change_types.NORMAL;
 
-    /** @type{boolean} whether the deletion mode is enabled or not */
-    export let deletion_mode = false;
-
-    /** @type{"Moved" | "Deleted" | "Normal"} the current media change type */
-    let media_current_change = media_change_types.NORMAL;
+        const event_dispatcher = createEventDispatcher();
+    
+    
+    
+    /*=====  End of Properties  ======*/
 
     onMount(() => {
         media_current_change = $media_changes_manager.getMediaChangeType(media.uuid);
-    })
+    });
+    
+    /*=============================================
+    =            Methods            =
+    =============================================*/
+    
+        const deleteMedia = () => {
+            if (media_current_change === media_change_types.DELETED) {
+                $media_changes_manager.unstageMediaDeletion(media.uuid);
+                media_current_change = media_change_types.NORMAL;
+                return;
+            }
 
-    const deleteMedia = () => {
-        if (media_current_change === media_change_types.DELETED) {
-            $media_changes_manager.unstageMediaDeletion(media.uuid);
-            media_current_change = media_change_types.NORMAL;
-            return;
+            media_current_change = media_change_types.DELETED;
+            $media_changes_manager.stageMediaDeletion(media);
+
+            let active_media = $current_category?.content[$active_media_index];
+            if (active_media?.uuid === media.uuid) {
+                active_media_change.set(media_current_change);
+            }
+        };
+
+        /**
+         * Handles the click event on the div.mg-thumbnail-wrapper element
+         * @param {MouseEvent} event
+         */
+        const emitImageSelected = event => {
+            if (deletion_mode) {
+                return deleteMedia();
+            }
+
+            event_dispatcher("image-selected", media);
         }
+    
+    
+    /*=====  End of Methods  ======*/
 
-        media_current_change = media_change_types.DELETED;
-        $media_changes_manager.stageMediaDeletion(media);
-
-        let active_media = $current_category?.content[$active_media_index];
-        if (active_media?.uuid === media.uuid) {
-            active_media_change.set(media_current_change);
-        }
-    };
-
-    const emitImageSelected = e => {
-        if (deletion_mode) {
-            return deleteMedia();
-        }
-
-        event_dispatcher("image-selected", media);
-    }
 </script>
 
 <div class="mg-thumbnail-wrapper" class:deleted-media={media_current_change === media_change_types.DELETED} on:click|stopPropagation={emitImageSelected}>
