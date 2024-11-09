588e4c34d40694800e97196afd3bda9fb544d2cb
Gerardo <theronin115@gmail.com>
2024-11-08 23:35:52 -0600

    fix: refactored the InnerCategoryView component, improving jsdoc annotations, readability and more common patterns

--- apps/libery_dungeon/src/pages/MediaViewer/sub-components/InnerCategoryView.svelte
+++ apps/libery_dungeon/src/pages/MediaViewer/sub-components/InnerCategoryView.svelte
@@ -9,8 +9,6 @@
         import { create_subcategory } from "@pages/MediaViewer/app_page_store";
         import { getCategory, createCategory } from "@models/Categories";
         import { categories_tree } from "@stores/categories_tree";
-        import FieldData from "@libs/FieldData";
-        import Input from "@components/Input/Input.svelte";
         import { current_cluster } from "@stores/clusters";
     /*=====  End of Imports  ======*/
     
@@ -35,12 +33,38 @@
         
         /** @type {boolean} whether to show inner categories or not */
         export let show_inner_categories = true;
-        
-        /** @type { boolean } if true, show the create subcategory input */
-        let show_create_subcategory_input = false;
 
-        /** @type { FieldData } data for the create subcategory input */
-        let create_subcategory_field = new FieldData("create-subcategory-input", /.+/, "New subcategory");
+        /* -------------------------- sub-category creation ------------------------- */
+
+            /** @type { boolean } if true, show the create subcategory input */
+            let show_create_subcategory_input = false;
+
+            /**
+             * The input field used to create a new subcategory
+             * @type {HTMLInputElement | null}
+             */
+            let the_subcategory_creation_input = null;
+
+            /**
+             * The subcategory new name.
+             * @type {string}
+             * @default ""
+             */
+            let subcategory_name = "";
+
+            /**
+             * Whether the new subcategory name is valid.
+             * @type {boolean} 
+             */
+            let new_subcategory_name_valid = false;
+
+            /**
+             * The subcategory creation form.
+             * @type {HTMLFormElement | null}
+             */
+            let the_subcategory_creation_form = null;
+
+        
 
         const dispatch = createEventDispatcher();
 
@@ -52,12 +76,7 @@
     /*=====  End of Properties  ======*/
 
     onMount(() => {
-        // PASS A GOD DAMN PROP TO CHANGE THIS STATE THEN MAKE A REACTIVE STATEMENT! this down below is horrible.
-        new_category_name_unsubscriber = create_subcategory.subscribe(value => {
-            if (value) {
-                handleCreateSubcategory();
-            }
-        });
+        new_category_name_unsubscriber = create_subcategory.subscribe(setSubCategoryCreationState);
     })
 
     onDestroy(() => {
@@ -70,7 +89,7 @@
     
         /**
          * Handles new subcategory creation.
-         * @param category_name
+         * @param {string} category_name
          */
         const createSubCategory = async category_name => {
             let category_complete_data = await getCategory(category.uuid);
@@ -85,56 +104,86 @@
             });
         }
 
-        const handleCreateSubcategory = () => {
-            create_subcategory_field.clear();
+        /**
+         * Sets the subcategory creation state.
+         * @param {boolean | null} enable
+         */
+        const setSubCategoryCreationState = enable => {
+            the_subcategory_creation_form?.reset();
 
-            show_create_subcategory_input = is_keyboard_selected;            
+            show_create_subcategory_input = is_keyboard_selected && (enable === true); // only if true, not count truethy values
         }    
 
-        const handleCategoryItemSelected = e => {
-            e.stopPropagation();
+        /**
+         * Creates a new subcategory with the subcategory_name if new_subcategory_name_valid is true.
+         * @returns {Promise<void>}
+         */
+        const createSubCategoryIfValid = async () => {
+            if (!new_subcategory_name_valid) return;
 
-            let propagate_uuid = category.uuid;
-            let propagate_name = category.name;
-            let shift_key = e.shiftKey;
+            subcategory_name = subcategory_name.trim();
 
-            if (!(e instanceof MouseEvent)) {
-                propagate_name = e.detail.category.name;
-                propagate_uuid = e.detail.category.uuid;
-                shift_key = e.detail.shift_key;
-            }
+            createSubCategory(subcategory_name);
 
-            dispatch("category-item-selected", {
-                category: {
-                    uuid: propagate_uuid,
-                    name: propagate_name
-                },
-                shift_key: shift_key
-            });
+            return;
         }
 
         /**
-         * @param { KeyboardEvent } e 
+         * @param {MouseEvent} e
          */
-        const handleCreateSubcategoryInputCommands = e => {
+        const handleCategoryItemClicked = e => {
+            e.stopPropagation();
+
+            let shift_key = e.shiftKey;
+
+            dispatch("category-item-selected", { category: category, shift_key: shift_key });
+        }
+
+        /**
+         * @param {KeyboardEvent} e 
+         */
+        const handleCreateSubcategoryInputKeypress = e => {
+        }
+
+        /**
+         * @param {KeyboardEvent} e 
+         */
+        const handleCreateSubcategoryInputKeydown = e => {
             if (e.key === "Enter" || (e.key === "e" && e.ctrlKey)) {
                 e.preventDefault();
 
-                const new_subcategory_name = create_subcategory_field.getFieldValue();
+                if ($create_subcategory === false) return;
 
-                if (create_subcategory === "") return;
+                create_subcategory.set(false);
 
+                createSubCategoryIfValid();
+            }
+
+            if (e.key === "Escape") {
+                the_subcategory_creation_input?.blur();
                 show_create_subcategory_input = false;
-                create_subcategory.set(null); // 🤮🤮🤮🤮🤮
-
-                createSubCategory(new_subcategory_name);
+                create_subcategory.set(false);
             }
         }
-            
+
+        /**
+         * @param {KeyboardEvent} event 
+         */
+        const handleCreateSubcategoryInputKeyup = event => {
+            if (the_subcategory_creation_input == null) return;
+
+            event.preventDefault();
+
+            if (the_subcategory_creation_input.validationMessage !== "") {
+                the_subcategory_creation_input.setCustomValidity("");
+            }
+
+            new_subcategory_name_valid = the_subcategory_creation_input.checkValidity();
+        }
 
         const toggleInnerCategories = async () => {
             if (inner_categories.length === 0 && show_inner_categories) {
-                inner_categories = await getShortCategoryTree(category.uuid);
+                inner_categories = await getShortCategoryTree(category.uuid, $current_cluster.UUID);
             }
 
             is_expanded = !is_expanded;
@@ -148,41 +197,49 @@
 <li id="inner-category-{category.uuid}" class="inner-category" class:keyboard-selected={is_keyboard_selected}>
     <div class="inner-category-wrapper" class:is-light={is_light} on:click={toggleInnerCategories}>
         {#if show_create_subcategory_input}
-            <!-- 
-                I deprecated the Input component in the previous century! DO NOT USE IT! the input element API now a day does everything this component was created for.
-             -->
-            <Input 
-                field_data={create_subcategory_field}
-                input_background="var(--grey)"
-                placeholder_color="var(--main-1)"
-                input_padding="calc(var(--vspacing-1) * .3) var(--vspacing-1)"
-                input_color="var(--main-5)"
-                font_size="var(--font-size-1)"
-                border_color="var(--main)"
-                onKeypress={handleCreateSubcategoryInputCommands}
-                isSquared={true}
-                autofocus={true}
-            />
+            <form action="none" class="subcategory-creation-form"
+                bind:this={the_subcategory_creation_form}
+            >
+                <label class="subcategory-creation-wrapper dungeon-input">
+                    <span class="dungeon-label">
+                        subcategory     
+                    </span>
+                    <input 
+                        bind:this={the_subcategory_creation_input}
+                        bind:value={subcategory_name}
+                        type="text"
+                        autocomplete="off"
+                        on:keydown={handleCreateSubcategoryInputKeydown}
+                        on:keyup={handleCreateSubcategoryInputKeyup}
+                        on:keypress={handleCreateSubcategoryInputKeypress}
+                        autofocus
+                        required
+                    >
+                </label>
+            </form>
         {:else}
-            <strong class="ic-category-name" on:click={handleCategoryItemSelected}>
+            <strong class="ic-category-name" on:click={handleCategoryItemClicked}>
                 {category.name}
             </strong>
             {#if category.fullpath != null && category.fullpath != InnerCategory.EMPTY_FULLPATH}
-            <i class="ic-category-fullpath">
-                {category.fullpath}
-            </i> 
+                <i class="ic-category-fullpath">
+                    {category.fullpath}
+                </i> 
             {/if}
         {/if}
     </div>
     {#if is_expanded && inner_categories.length > 0}
         <ul id="inner-categories-{category.uuid}">
             {#each inner_categories as ic}
-                <InnerCategoryView category={ic} is_light={!is_light} on:category-item-selected={handleCategoryItemSelected} />
+                <svelte:component this={InnerCategoryView} 
+                    category={ic} 
+                    is_light={!is_light} 
+                    on:category-item-selected
+                />
             {/each}
         </ul>
     {/if}
 </li>
-
 
 <style>
     .inner-category-wrapper {
