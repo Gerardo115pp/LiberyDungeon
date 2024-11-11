6c81bbbcb364f80d5a7937445e3a37d4549eccb2
Gerardo <theronin115@gmail.com>
2024-11-11 03:28:13 -0600

    fix: add JSDoc comments for parameters and return types in ChanTrackedBoard, ChanCatalogThread, ChanThreadReply, and ChanThread classes

--- apps/libery_dungeon/src/models/4Chan.js
+++ apps/libery_dungeon/src/models/4Chan.js
@@ -22,6 +22,11 @@ import {
 */
 
 export class ChanTrackedBoard {
+
+    /**
+     * @param {string} board_name 
+     * @param {string} description 
+     */
     constructor(board_name, description) {
         this.board_name = board_name;
         this.description = description;
@@ -29,6 +34,21 @@ export class ChanTrackedBoard {
 }
 
 export class ChanCatalogThread {
+    /**
+     * 
+     * @param {ChanCatalogThreadParams} param0 
+     * @typedef {Object} ChanCatalogThreadParams
+     * @property {string} uuid
+     * @property {number} date
+     * @property {string} file
+     * @property {number} responses
+     * @property {number} images
+     * @property {string} teaser
+     * @property {string} image_url
+     * @property {number} teaser_thumb_width
+     * @property {number} teaser_thumb_height
+     * @property {string} board_name
+     */
     constructor({uuid, date, file, responses, images, teaser, image_url, teaser_thumb_width, teaser_thumb_height, board_name}) {
         this.uuid = uuid;
         this.date = date;
@@ -55,9 +75,10 @@ export class ChanCatalogThread {
     /**
      * Sends a request to the server to download the thread.
      * @param {string} category_name The category to download the thread to.
-     * @param {string} [parent_uuid] The parent category's uuid.
-     * @returns {Promise<string>} The response from the server.
-    */
+     * @param {string} parent_uuid The parent category's uuid.
+     * @param {string} cluster_uuid The cluster uuid.
+     * @returns {Promise<string | null>} The response from the server.
+     */
     async download(category_name, parent_uuid, cluster_uuid) {
         const request = new PostThreadDownloadRequest(this.uuid, this.board_name, category_name, parent_uuid, cluster_uuid);
 
@@ -71,8 +92,6 @@ export class ChanCatalogThread {
 
         return download_uuid;
     }
-
-
     
     /**
      * Checks if the thread has been downloaded.
@@ -98,15 +117,28 @@ export class ChanCatalogThread {
 }
 
 export class ChanThreadReply {
+
+    /**
+     * @param {ChanThreadReplyParams} param0
+     * @typedef {Object} ChanThreadReplyParams
+     * @property {string} uuid
+     * @property {string} message
+     * @property {string} date
+     * @property {string} file
+     * @property {string} thumbnail_url
+     */
     constructor({uuid, message, date, file, thumbnail_url}) {
         this.uuid = uuid;
         this.message = message;
+
+        let numeric_date = NaN;
+
         if (isNaN(+date)) {
-            date = parseInt(date);
-            date = isNaN(date) ? new Date().getTime() : date;
+            numeric_date = parseInt(date);
+            numeric_date = isNaN(numeric_date) ? new Date().getTime() : numeric_date;
         }
 
-        this.date = date;
+        this.date = numeric_date;
         this.file = file;
         this.thumbnail_url = thumbnail_url;
     }
@@ -127,13 +159,28 @@ export class ChanThreadReply {
 }
 
 export class ChanThread {
+
+    /**
+     * @param {ChanThreadParams} param0
+     * @typedef {Object} ChanThreadParams
+     * @property {string} uuid
+     * @property {string} date
+     * @property {string} file
+     * @property {string} title
+     * @property {string} description
+     * @property {string} cover_image_url
+     */
     constructor({uuid, date, file, title, description, cover_image_url}) {
         this.uuid = uuid;
+
+        let numeric_date = NaN;
+
         if (isNaN(+date)) {
-            date = parseInt(date);
-            date = isNaN(date) ? new Date().getTime() : date;
+            numeric_date = parseInt(date);
+            numeric_date = isNaN(numeric_date) ? new Date().getTime() : numeric_date;
         }
-        this.date = date;
+
+        this.date = numeric_date;
         this.file = file;
         this.title = title;
         this.description = description;
@@ -171,11 +218,18 @@ export const getTrackedBoards = async () => {
     return boards;
 }
 
+/**
+ * @param {string} board_name 
+ * @returns {Promise<ChanCatalogThread[]>}
+ */
 export const getBoardCatalog = async (board_name) => {
     const request = new GetChanCatalogRequest(board_name);
 
     const response = await request.do();
 
+    /**
+     * @type {ChanCatalogThread[]}
+     */
     const threads = [];
 
     for (let thread of response.data) {
@@ -191,7 +245,7 @@ export const getBoardCatalog = async (board_name) => {
  * Gets the content of a thread.
  * @param {string} board_name The board name.
  * @param {string} thread_uuid The thread uuid.
- * @returns {Promise<ChanThread>} The thread.
+ * @returns {Promise<ChanThread | null>} The thread.
  */
 export const getThreadContent = async (board_name, thread_uuid) => {
     const request = new GetChanThreadRequest(thread_uuid, board_name);
