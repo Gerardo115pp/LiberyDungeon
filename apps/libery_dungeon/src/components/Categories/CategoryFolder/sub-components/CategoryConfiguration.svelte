11fe27f1dbbb71672ad3feca7552ac4d321f1494
Gerardo <theronin115@gmail.com>
2024-11-14 05:34:04 -0600

    feat: implement thumbnail change handling in CategoryConfiguration component

--- apps/libery_dungeon/src/components/Categories/CategoryFolder/sub-components/CategoryConfiguration.svelte
+++ apps/libery_dungeon/src/components/Categories/CategoryFolder/sub-components/CategoryConfiguration.svelte
@@ -3,6 +3,10 @@
     import SettingEntry from '@components/Informative/DataEntries/SettingEntry.svelte';
     import { slide } from 'svelte/transition';
     import { quartOut } from 'svelte/easing';
+    import { getMediaByUUID, getMediaIdentityByUUID } from '@models/Medias';
+    import { LabeledError, VariableEnvironmentContextError } from '@libs/LiberyFeedback/lf_models';
+    import { lf_errors } from '@libs/LiberyFeedback/lf_errors';
+    import { changeCategoryThumbnail } from '@models/Categories';
 
     /*=============================================
     =            Properties            =
@@ -17,8 +21,83 @@
         const category_setting_ids = {
             CATEGORY_THUMBNAIL: 'category_thumbnail_uuid',
         }
+
+        
+        /*----------  Event handlers  ----------*/
+        
+            /**
+             * Handler for the thumbnail-change event.
+             * @type {import('./category_folder_subs').CategoryConfig_ThumbnailChanged}
+             */ 
+            export let on_thumbnail_change = updated_category => {};
     
     /*=====  End of Properties  ======*/
+    
+    /*=============================================
+    =            Methods            =
+    =============================================*/
+    
+        /**
+         * Handles new setting changes.
+         * @type {import('@components/Informative/DataEntries/data_entries').SettingChangeHandler}
+         */
+        const handleSettingChanges = (setting_id, new_value) => {
+            switch (setting_id) {
+            case category_setting_ids.CATEGORY_THUMBNAIL: 
+                handleThumbnailChanges(new_value);
+                return;
+            default:
+                console.error(`In CategoryConfiguration.handleSettingChanges: no setting called ${setting_id}`);
+                return;
+            }
+        }
+
+        /**
+         * Handles thumbnail changes.
+         * @param {string} new_thumbnail_uuid
+         * @returns {Promise<void>}
+         */
+        const handleThumbnailChanges = async (new_thumbnail_uuid) => {
+
+            const thumbnail_media = await getMediaIdentityByUUID(new_thumbnail_uuid);
+
+            if (thumbnail_media == null) {
+                const variable_environment = new VariableEnvironmentContextError("In CategoryConfiguration.handleThumbnailChanges")
+
+                variable_environment.addVariable("category_uuid", the_inner_category.uuid);
+                variable_environment.addVariable("current_thumbnail_uuid", the_inner_category.ThumbnailUUID);
+                variable_environment.addVariable("new_thumbnail_uuid", new_thumbnail_uuid);
+
+                const labeled_err = new LabeledError(variable_environment, `Seems like media '${new_thumbnail_uuid}' doesn't actually exist.`, lf_errors.ERR_HUMAN_ERROR);
+
+                labeled_err.alert();
+
+                return;
+            }
+
+            const thumbnail_set = await changeCategoryThumbnail(the_inner_category.uuid, new_thumbnail_uuid);
+
+            if (!thumbnail_set) {
+                const variable_environment = new VariableEnvironmentContextError("In CategoryConfiguration.handleThumbnailChanges")
+
+                variable_environment.addVariable("category_uuid", the_inner_category.uuid);
+                variable_environment.addVariable("current_thumbnail_uuid", the_inner_category.ThumbnailUUID);
+                variable_environment.addVariable("new_thumbnail_uuid", new_thumbnail_uuid);
+
+                const labeled_err = new LabeledError(variable_environment, `Sorry, there was an error setting the thumbnail to '${new_thumbnail_uuid}'.`, lf_errors.ERR_PROCESSING_ERROR);
+
+                labeled_err.alert();
+
+                return;
+
+            }
+
+            the_inner_category.setThumbnail(thumbnail_media);
+
+            on_thumbnail_change(the_inner_category);
+        }
+    
+    /*=====  End of Methods  ======*/
     
 </script>
 
@@ -46,6 +125,7 @@
                 font_size="var(--cacow-font-size)"
                 information_entry_label="thumbnail"
                 information_entry_value={the_inner_category.ThumbnailUUID}
+                on_setting_change={handleSettingChanges}
             /> 
         </div>
     </menu>
