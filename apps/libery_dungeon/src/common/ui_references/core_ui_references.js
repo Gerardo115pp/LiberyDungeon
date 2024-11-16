f6a7adec2a15ef2f37244e3e59f45911f10c9c09
Gerardo <theronin115@gmail.com>
2024-11-16 06:21:31 -0600

    feat: add core UI references for dungeon and media categories

---
+++ apps/libery_dungeon/src/common/ui_references/core_ui_references.js
@@ -0,0 +1,7 @@
+import { UIReference } from "@libs/LiberyFeedback/lf_models";
+
+export const core_dungeon_ui_references = {
+    CATEGORY_CLUSTER: new UIReference("dungeon", "dungeons"),
+    CATEGORY: new UIReference("cell", "cells"),
+    MEDIA: new UIReference("media", "medias"),
+}
