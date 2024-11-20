308adb31ba08fea0e0d9aae3048620a04006ecc0
Gerardo <theronin115@gmail.com>
2024-11-20 01:05:23 -0600

    feat: add UI reference tags for pandasworld in dungeon_tags_references

---
+++ apps/libery_dungeon/src/common/ui_references/dungeon_tags_references.js
@@ -0,0 +1,6 @@
+import { UIReference } from "@libs/LiberyFeedback/lf_models";
+
+export const ui_pandasworld_tag_references = {
+    TAG: new UIReference("value", "values"),
+    TAG_TAXONOMY: new UIReference("attribute", "attributes")
+}
