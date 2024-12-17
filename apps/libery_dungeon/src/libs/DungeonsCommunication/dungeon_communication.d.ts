a61dbf261861c035e677ca1e05502f3aeb5159d8
Gerardo <theronin115@gmail.com>
2024-12-17 00:42:12 -0600

    feat: updated the PaginatedResponse type on the libery_dungeon app to include the new fields of the paginated struct of the server

--- apps/libery_dungeon/src/libs/DungeonsCommunication/dungeon_communication.d.ts
+++ apps/libery_dungeon/src/libs/DungeonsCommunication/dungeon_communication.d.ts
@@ -1,10 +1,10 @@
 export interface Stringable {
     toString(): string;
 }
-
 export type PaginatedResponse<T> = {
     page: number;
     page_size: number;
     total_pages: number;
+    total_items: number;
     content: T[];
 }
