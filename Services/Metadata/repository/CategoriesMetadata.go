6f9d4d0617cb4f9b6efb600e065ad56c912488a7
Gerardo <theronin115@gmail.com>
2025-01-10 00:09:44 -0600

    feat: add CategoriesConfigurationRepository

---
+++ Services/Metadata/repository/CategoriesMetadata.go
@@ -0,0 +1,9 @@
+package repository
+
+import "libery-metadata-service/models"
+
+type CategoriesConfigurationRepository interface {
+	CreateCategoryConfig(category_uuid string) *models.CategoryConfig // Creates a new category config with default values.
+	GetCategoryConfig(category_uuid string) *models.CategoryConfig
+	UpdateCategoryConfig(category_config *models.CategoryConfig) error
+}
