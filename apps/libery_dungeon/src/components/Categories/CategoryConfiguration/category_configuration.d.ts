c6d147996a91bb34c459e316369377a061f91d6b
Gerardo <theronin115@gmail.com>
2025-01-11 04:27:56 -0600

    feat: add new types for billboard media addition and removal in CategoryConfiguration

--- apps/libery_dungeon/src/components/Categories/CategoryConfiguration/category_configuration.d.ts
+++ apps/libery_dungeon/src/components/Categories/CategoryConfiguration/category_configuration.d.ts
@@ -1,3 +1,8 @@
 import { InnerCategory } from "@models/Categories"
+import { MediaIdentity } from "@models/Medias";
 
 export type CategoryConfig_ThumbnailChanged = (updated_category: InnerCategory) => void;
+
+export type CategoryConfig_BillboardMediaAdded = (media_identity: MediaIdentity) => void;
+
+export type CategoryConfig_BillboardMediaRemoved = (media_uuid: string) => void;
