597ffd25f72c889bc5d08459c577a37eac5868b5
Gerardo <theronin115@gmail.com>
2025-01-11 06:01:45 -0600

    feat: add ParseTagIDsListParams function to parse tag IDs from HTTP request

--- Services/Shared/go_shared/communication/service_requests/metadata_requests/metadata_http_requests.go
+++ Services/Shared/go_shared/communication/service_requests/metadata_requests/metadata_http_requests.go
@@ -1,5 +1,10 @@
 package metadata_requests
 
+import (
+	dungeon_helpers "libery-dungeon-libs/helpers"
+	"net/http"
+)
+
 // -------------------- Dungeon tags --------------------
 
 type MultiTagEntitiesRequest struct {
@@ -18,6 +23,27 @@ type TagEntitiesRequest struct {
 	DungeonTagID  int      `json:"tag_id"`
 	EntityType    string   `json:"entity_type"`
 	EntitiesUUIDs []string `json:"entities_uuids"`
+}
+
+type TagListRequest struct {
+	TagList []int `json:"tag_list"`
+}
+
+func ParseTagIDsListParams(request *http.Request) (*TagListRequest, error) {
+	const (
+		tag_list_key string = "tags"
+	)
+
+	var request_body *TagListRequest = new(TagListRequest)
+
+	tag_list, err := dungeon_helpers.ParseQueryParameterAsIntSlice(request, tag_list_key)
+	if err != nil {
+		return nil, err
+	}
+
+	request_body.TagList = tag_list
+
+	return request_body, nil
 }
 
 // -------------------- Categories config --------------------
