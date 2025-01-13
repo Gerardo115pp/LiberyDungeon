d27e47ad766f5aedec5ce02ebd660ed794821d86
Gerardo <theronin115@gmail.com>
2025-01-13 17:47:44 -0600

    feat: Add database schema for videos and video moments with foreign key constraints

---
+++ database/schemas/video_moments.sql
@@ -0,0 +1,16 @@
+PRAGMA foreign_keys=ON;
+
+DROP TABLE IF EXISTS `videos`;
+CREATE TABLE IF NOT EXISTS `videos` (
+    `uuid` TEXT PRIMARY KEY,
+    `title` TEXT NOT NULL,
+    `cluster_uuid` TEXT NOT NULL,
+);
+
+DROP TABLE IF EXISTS `video_moments`;
+CREATE TABLE IF NOT EXISTS `video_moments` (
+    `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `video_uuid` TEXT NOT NULL,
+    `moment_time` INTEGER NOT NULL,
+    FOREIGN KEY(`video_uuid`) REFERENCES `videos`(`uuid`) ON DELETE CASCADE
+);
