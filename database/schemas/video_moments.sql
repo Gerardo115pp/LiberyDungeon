3af92d21f7be6500dcc5d7e1dd947bd91e055d46
Gerardo <theronin115@gmail.com>
2025-01-13 18:09:07 -0600

    fix: Remove title column from videos table in video moments schema

--- database/schemas/video_moments.sql
+++ database/schemas/video_moments.sql
@@ -3,7 +3,6 @@ PRAGMA foreign_keys=ON;
 DROP TABLE IF EXISTS `videos`;
 CREATE TABLE IF NOT EXISTS `videos` (
     `uuid` TEXT PRIMARY KEY,
-    `title` TEXT NOT NULL,
     `cluster_uuid` TEXT NOT NULL,
 );
 
