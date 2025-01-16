cb2996f2194e82c2d3eb76826dbf6f035ea92f0f
Gerardo <theronin115@gmail.com>
2025-01-15 18:10:35 -0600

    feat: add functionality to update video moment title and time by ID

--- apps/libery_dungeon/src/libs/DungeonsCommunication/services_requests/metadata_requests/video_moments_requests.js
+++ apps/libery_dungeon/src/libs/DungeonsCommunication/services_requests/metadata_requests/video_moments_requests.js
@@ -67,6 +67,9 @@ export class PostVideoMomentsRequest {
     }
 }
 
+/**
+ * Returns all the video moments that have been saved for a video.
+ */
 export class GetVideoMomentsRequest {
 
     static endpoint = `${metadata_server}/video-moments/video-moments`;
@@ -113,5 +116,60 @@ export class GetVideoMomentsRequest {
         }
 
         return new HttpResponse(response, video_moments);
+    }
+}
+
+/**
+ * Updates the title and/or the time of a video moment by its id.
+ */
+export class PutVideoMomentDataRequest {
+
+    static endpoint = `${metadata_server}/video-moments/moment`;
+
+    /**
+     * @param {number} id - the id of the video moment.
+     * @param {number} moment_time
+     * @param {string} moment_title
+     */
+    constructor(id, moment_time, moment_title) {
+        this.id = id;
+        this.moment_time = moment_time;
+        this.moment_title = moment_title;
+    }
+
+    toJson = attributesToJson.bind(this);
+
+    /**
+     * @returns {Promise<HttpResponse<boolean>>}
+     */
+    do = async () => {
+        /**
+         * @type {boolean}
+         */
+        let successful_update = false;
+
+        /**
+         * @type {Response}
+         */
+        let response;
+
+        const url = new URL(PutVideoMomentDataRequest.endpoint, globalThis.location.origin);
+
+        try {
+            response = await fetch(url, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: this.toJson()
+            });
+
+            successful_update = response.ok;
+        } catch (err) {
+            console.error(`In @libs/DungeonsCommunication/services_requests/metadata_requests/video_moments_requests PutVideoMomentDataHandler.do: ${err}`);
+            throw err;
+        }
+
+        return new HttpResponse(response, successful_update);
     }
 }
