d1e20d864ce17a6006bc9589a2d71da8ce6cd79a
Gerardo <theronin115@gmail.com>
2025-01-16 21:47:09 -0600

    feat: add VideoMomentsTool and VideoMomentIdentityView components for displaying and interacting with video moments in the media explorer

---
+++ apps/libery_dungeon/src/pages/MediaExplorer/sub-components/VideoMomentsTool/VideoMomentsTool.svelte
@@ -0,0 +1,147 @@
+<script>
+    import { current_cluster } from "@stores/clusters";
+    import { onMount } from "svelte";
+    import { VideoMomentIdentity } from "@models/Metadata";
+    import VideoMomentIdentityView from "./sub-components/VideoMomentIdentityView.svelte";
+
+    /*=============================================
+    =            Properties            =
+    =============================================*/
+    
+        /**
+         * All the video moments in the cluster.
+         * @type {VideoMomentIdentity[]}
+         */
+        let cluster_video_moments = []; 
+    
+    /*=====  End of Properties  ======*/
+
+    onMount(() => {
+        console.log("VideoMomentsTool mounted");
+       
+        loadClusterVideoMoments();
+    })
+    
+    
+    /*=============================================
+    =            Methods            =
+    =============================================*/
+    
+        /**
+         * Loads all the video moments from the current cluster 
+         * into the cluster_video_moments array 
+         * @returns {Promise<void>} 
+         */ 
+        async function loadClusterVideoMoments() {
+            const cluster = $current_cluster;
+            if (cluster === null) {
+                console.error("@pages/MediaExplorer/sub-components/VideoMomentsTool/VideoMomentsTool.loadClusterVideoMoments: cluster is null");
+                return;
+            }
+
+            if (cluster.isVideoMomentsCacheEmpty()) {
+                await cluster.loadAllVideoMoments();
+            }
+
+            const video_moments = cluster.getVideoMomentsCacheItems();
+            const videos_uuids = new Set(video_moments.map(vm => vm.VideoUUID));
+            console.log("videos_uuids", videos_uuids);
+            console.log("video_moments", video_moments);
+
+            const video_identities = await cluster.getClusterMedias(Array.from(videos_uuids)); 
+            
+            const video_identities_map = new Map();
+
+            for (let identity of video_identities) {
+                video_identities_map.set(identity.Media.uuid, identity);
+            }
+
+            const video_moment_identities = [];
+
+            for (let moment of video_moments) {
+                const video_identity = video_identities_map.get(moment.VideoUUID);
+
+                if (video_identity === undefined) {
+                    throw new Error(`@pages/MediaExplorer/sub-components/VideoMomentsTool/VideoMomentsTool.loadClusterVideoMoments: video identity not found for video uuid ${moment.VideoUUID}`);
+                }
+
+                const new_video_moment_identity = new VideoMomentIdentity(video_identity, moment);
+
+                video_moment_identities.push(new_video_moment_identity);
+            }
+
+            cluster_video_moments = video_moment_identities;
+        }
+    
+    /*=====  End of Methods  ======*/
+    
+    
+    
+</script>
+
+<dialog open id="dungeon-video-moments-explorer-tool"
+    class="libery-dungeon-window"
+>
+    <header id="diviext-header">
+        <h4 id="diviext-tool-title">
+            Video moments
+        </h4>
+        <p class="dungeon-description">
+            This is a list of all the video moments you have <strong>save in this cluster</strong>. Click on any of them to load the video on that video moment.
+        </p>
+    </header>
+    <div class="diviext-moments-wrapper">
+        <ul class="diviext-moments dungeon-scroll">
+            {#each cluster_video_moments as video_moment}
+                <li class="diviext-moment-wrapper">
+                    <VideoMomentIdentityView 
+                        the_video_moment_identity={video_moment}
+                    />
+                </li>
+            {/each}
+        </ul>
+    </div>
+</dialog>
+
+<style>
+    #dungeon-video-moments-explorer-tool {
+        display: flex;
+        flex-direction: column;
+        container-type: size;
+        background: var(--grey-t);
+        width: 100cqw;
+        aspect-ratio: 7 / 8;
+        row-gap: var(--spacing-3);
+        padding: var(--spacing-3);
+        backdrop-filter: var(--backdrop-filter-blur);
+    }
+
+    
+    /*=============================================
+    =            header            =
+    =============================================*/
+    
+        header#diviext-header {
+            display: flex;
+            flex-direction: column;
+            row-gap: var(--spacing-1);
+            color: var(--grey-2);
+        } 
+    
+    /*=====  End of header  ======*/
+
+    ul.diviext-moments {
+        display: flex;
+        flex-direction: column;
+        overflow-y: auto;
+        overflow-x: hidden;
+        overscroll-behavior: contain;
+        background: hsl(from var(--grey-t) h s l / 0.7);
+        border-radius: var(--rounded-box-border-radius);
+        height: 70cqh;
+
+        & > li.diviext-moment-wrapper:not(:last-child) {
+            border-bottom: 1px solid var(--main-dark-color-7);
+        }
+    }
+</style>
---
+++ apps/libery_dungeon/src/pages/MediaExplorer/sub-components/VideoMomentsTool/sub-components/VideoMomentIdentityView.svelte
@@ -0,0 +1,70 @@
+<script>
+    import { navigateToMediaViewer } from '@libs/NavigationUtils';
+
+    /*=============================================
+    =            Propertie            =
+    =============================================*/
+    
+        /**
+         * @type {import('@models/Metadata').VideoMomentIdentity}
+         */ 
+        export let the_video_moment_identity;
+    
+    /*=====  End of Propertie  ======*/
+
+    
+    /*=============================================
+    =            Methods            =
+    =============================================*/
+    
+        /**
+         * Handles the click event on the video moment item.
+         * @param {MouseEvent} event
+         */ 
+        const handleVideoMomentClick = event => {
+
+            navigateToMediaViewer(the_video_moment_identity.MediaIdentity.CategoryUUID, {
+                media_uuid: the_video_moment_identity.MediaIdentity.Media.uuid,
+                time: the_video_moment_identity.VideoMoment.DecodedTime
+            });
+        }
+    
+    /*=====  End of Methods  ======*/
+    
+    
+</script>
+
+<!-- svelte-ignore a11y-interactive-supports-focus -->
+<div class="diviext-moment-identity" 
+    role="button"
+    on:click={handleVideoMomentClick}
+>
+    <p>
+        <strong>{the_video_moment_identity.MediaIdentity.Media.MediaName}</strong> ―
+        {the_video_moment_identity.VideoMoment.Title}
+    </p>
+</div>
+
+<style>
+    .diviext-moment-identity {
+        color: var(--grey-1);
+        background: hsl(from var(--grey-9) h s l / 0.8);
+        padding-inline: 3em;
+        padding-block: .75em;
+        transition: background 0.3s ease-out;
+
+        & > p {
+            font-size: var(--font-size-1);
+        }
+
+        & > p strong {
+            color: var(--main);
+        }
+
+        &:hover {
+            cursor: pointer;
+            background: hsl(from var(--grey-7) h s l / 0.9);
+        }
+    }
+</style>
+
