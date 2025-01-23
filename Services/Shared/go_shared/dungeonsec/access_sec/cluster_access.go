503e9f0d4027716db5bb197b476cb7e8ebc988c0
Gerardo <theronin115@gmail.com>
2025-01-22 19:49:54 -0600

    feat: Added a functionality to retrieve clusters from cluster_access_tokens using a given cluster uuid.

--- Services/Shared/go_shared/dungeonsec/access_sec/cluster_access.go
+++ Services/Shared/go_shared/dungeonsec/access_sec/cluster_access.go
@@ -1,6 +1,7 @@
 package access_sec
 
 import (
+	"errors"
 	"fmt"
 	"libery-dungeon-libs/dungeonsec/dungeon_secrets"
 	dungeon_models "libery-dungeon-libs/models"
@@ -10,18 +11,11 @@ import (
 
 const cluster_access_prefix string = "cluster-access"
 
-func getClusterAccessCookieName(cluster *dungeon_models.CategoryCluster) string {
-	if cluster == nil {
-		return ""
-	}
+var (
+	MissingClusterAccessErr error = errors.New("Request has no access to the given cluster uuid")
+)
 
-	return fmt.Sprintf("%s-%s", cluster_access_prefix, cluster.Uuid)
-}
-
-func getClusterAccessTokenExpiration() time.Time {
-	return time.Now().Add(time.Hour * 24)
-}
-
+// Creates a new cluster_access_token cookie for a given cluster
 func createClusterAccessCookie(cluster_instance *dungeon_models.CategoryCluster) (*http.Cookie, error) {
 	if cluster_instance == nil {
 		return nil, fmt.Errorf("cluster_instance is nil")
@@ -52,6 +46,74 @@ func createClusterAccessCookie(cluster_instance *dungeon_models.CategoryCluster)
 	return &access_cookie, nil
 }
 
+// Returns the name a cluster_access_token cookie for a given cluster must have
+func getClusterAccessCookieName(cluster *dungeon_models.CategoryCluster) string {
+	if cluster == nil {
+		return ""
+	}
+
+	return fmt.Sprintf("%s-%s", cluster_access_prefix, cluster.Uuid)
+}
+
+// Returns a cluster access cookie for the given cluster id in the given request.
+func getClusterAccessCookie(cluster_uuid string, request *http.Request) *http.Cookie {
+	if cluster_uuid == "" || request == nil {
+		return nil
+	}
+
+	access_cookie_name := getClusterAccessCookieName(&dungeon_models.CategoryCluster{Uuid: cluster_uuid})
+
+	access_cookie, err := request.Cookie(access_cookie_name)
+	if err != nil {
+		return nil
+	}
+
+	return access_cookie
+}
+
+// Returns the time at which a cluster_access_token should expire from the time of invocation
+func getClusterAccessTokenExpiration() time.Time {
+	return time.Now().Add(time.Hour * 24)
+}
+
+/** Returns cluster object for the given cluster_id that was signed on a given request. Or nil if:
+ * The request had no matching cookie
+ * The token on the cookie was expired
+ * The signature on the token as invalid
+ */
+func GetSignedClusterOnRequest(cluster_uuid string, request *http.Request) (*dungeon_models.CategoryCluster, error) {
+	if cluster_uuid == "" || request == nil {
+		return nil, fmt.Errorf("cluster_uuid or request is nil")
+	}
+
+	cluster_access_cookie := getClusterAccessCookie(cluster_uuid, request)
+	if cluster_access_cookie == nil {
+		return nil, MissingClusterAccessErr
+	}
+
+	category_cluster, err := dungeon_models.GetCategoriesClusterFromToken(cluster_access_cookie.Value, dungeon_secrets.GetDungeonJwtSecret())
+	if err != nil {
+		return nil, errors.Join(err, MissingClusterAccessErr)
+	}
+
+	return category_cluster, nil
+}
+
+// Returns whether the given request has cluster_access_token matching the cluster_id.
+func RequestHasClusterAccess(cluster_id string, request *http.Request) bool {
+	if cluster_id == "" || request == nil {
+		return false
+	}
+
+	access_cookie := getClusterAccessCookie(cluster_id, request)
+	if access_cookie == nil {
+		return false
+	}
+
+	return true
+}
+
+// Adds a brand new signed cluster_access_token for the given cluster as a cookie to the given response writer.
 func SignClusterAccessOnRequest(cluster *dungeon_models.CategoryCluster, response http.ResponseWriter) error {
 	if cluster == nil {
 		return fmt.Errorf("cluster or request is nil")
