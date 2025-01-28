d79dcafe34b1077a70c1f41524fe0dc9eb2e8e3c
Gerardo <theronin115@gmail.com>
2025-01-27 21:42:33 -0600

    feat: Slightly improved the logic of the nav menu component. this is a preparation for a larger refactor.

--- apps/libery_dungeon/src/components/Navbar/sub-components/PageNavMenu.svelte
+++ apps/libery_dungeon/src/components/Navbar/sub-components/PageNavMenu.svelte
@@ -2,23 +2,75 @@
     import MediaExplorerNp from "@pages/MediaExplorer/NavPanel/MediaExplorerNP.svelte"
     import MediaViewerNp from "@pages/MediaViewer/NavPanel/MediaViewerNP.svelte";
     import { page } from "$app/stores";
+    import { onMount, onDestroy } from "svelte";
+    import { browser } from "$app/environment";
+
+    
+    /*=============================================
+    =            Properties            =
+    =============================================*/
+    
+        const route_names = {
+            MEDIA_VIEWER: /\/media\-viewer\/[a-zA-Z\d]{40}(\/\d+)?/,
+            MEDIA_EXPLORER: /\/dungeon\-explorer\/[a-zA-Z\d]{40}$/,
+            DEFAULT: /^\//
+        }
+
+        /**
+         * @type {import('svelte').ComponentType<import('svelte').SvelteComponent> | null}
+         */
+        let current_component = null;   
+
+        let page_change_unsubscriber = () => {};
+    
+    /*=====  End of Properties  ======*/
+    
+    
 
 
-    const route_names = {
-        MEDIA_VIEWER: /\/media\-viewer\/[a-zA-Z\d]{40}(\/\d+)?/,
-        MEDIA_EXPLORER: /\/dungeon\-explorer\/[a-zA-Z\d]{40}$/,
-        DEFAULT: /^\//
-    }
+    onMount(() => {
+        page_change_unsubscriber = page.subscribe(determinePageComponent);
+    });
+
+    onDestroy(() => {
+        if (!browser) return;
+
+        page_change_unsubscriber();
+    });
+    
+    /*=============================================
+    =            Methods            =
+    =============================================*/
+    
+        /**
+         * determines the correct page navbar component to use based on the current page route.
+         * @returns {void}
+         */  
+        const determinePageComponent = () => {
+            let new_page_nav_component = null;
+
+            if (route_names.MEDIA_VIEWER.test($page.url.pathname)) {
+                new_page_nav_component = MediaViewerNp;
+            }
+
+            if (route_names.MEDIA_EXPLORER.test($page.url.pathname)) {
+                new_page_nav_component = MediaExplorerNp;
+            }
+
+            current_component = new_page_nav_component;
+        }
+        
+        
+
+    /*=====  End of Methods  ======*/
+    
+    
     
 </script>
 
 <div id="page-navmenu-wrapper">
-    {#if route_names.MEDIA_VIEWER.test($page.url.pathname)}
-        <MediaViewerNp />
-    {:else if route_names.MEDIA_EXPLORER.test($page.url.pathname)}
-        <MediaExplorerNp />
-    {:else if route_names.DEFAULT.test($page.url.pathname)}
-        <MediaExplorerNp />
+    {#if current_component !== null}
+        <svelte:component this={current_component} />
     {/if}
 </div>
 
