3cdc5c1f5e904fef37dbd70858854d1cbaea0e68
Gerardo <theronin115@gmail.com>
2025-06-27 01:54:46 -0600

    feat: extend HTMLElementEventMap to include altered media change state event

--- apps/libery_dungeon/src/pages/MediaExplorer/sub-components/MediaExplorerGallery/media_explorer_gallery.d.ts
+++ apps/libery_dungeon/src/pages/MediaExplorer/sub-components/MediaExplorerGallery/media_explorer_gallery.d.ts
@@ -1,5 +1,12 @@
 import type { OrderedMedia } from "@models/Medias";
+import type { MediaChangeType } from "@models/WorkManagers";
+import { meg_media_change_state_event_names } from "./me_gallery_state";
 
 export type ObserveMEGalleryCallback = (item: Element, media_item: OrderedMedia) => void;
 export type UnobserveMEGalleryCallback = (item: Element, media_item: OrderedMedia) => void;
 
+declare global {
+    interface HTMLElementEventMap {
+        [meg_media_change_state_event_names.ALTERED_MEDIA_CHANGE_STATE]: CustomEvent<MediaChangeType>;
+    }
+}
